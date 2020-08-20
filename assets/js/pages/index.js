7fc826a712ac1e82d218875a67ae3faf14fcfbe8
densyy <densyy@hotmail.com>
2020-08-20 00:03:55 -0300

    layout css base

--- assets/js/pages/index.js
+++ assets/js/pages/index.js
@@ -7,10 +7,10 @@ import {gerarArquivoRepository} from '../libs/repository.js'
 /* Start */
 
 const maiusculoPlural = 'CLIENTES'
-const minusculoSingularAcento = 'cliente'
 
 const minusculoPlural = 'clientes'
 const minusculoSingular = 'cliente'
+const minusculoSingularAcento = 'cliente'
 
 const tituloPlural = 'Clientes'
 const tituloSingular = 'Cliente'
@@ -18,10 +18,33 @@ const tituloSingularAcento = 'Cliente'
 
 const artigo = 'o'
 
-const conteudoArquivoRota = gerarArquivoRoute(maiusculoPlural, minusculoPlural, tituloSingular)
-const conteudoArquivoController = gerarArquivoController(maiusculoPlural, minusculoPlural, minusculoSingular, tituloSingular, minusculoSingularAcento, tituloSingularAcento, artigo)
-const conteudoArquivoRepository = gerarArquivoRepository(maiusculoPlural, minusculoSingular, tituloPlural, tituloSingular)
+const conteudoArquivoRota = gerarArquivoRoute(
+  maiusculoPlural,
+  minusculoPlural,
+  tituloSingular
+)
 
-console.log(conteudoArquivoRota)
-console.log(conteudoArquivoController)
-console.log(conteudoArquivoRepository)
+const conteudoArquivoController = gerarArquivoController(
+  maiusculoPlural,
+  minusculoPlural,
+  minusculoSingular,
+  tituloSingular,
+  minusculoSingularAcento,
+  tituloSingularAcento,
+  artigo
+)
+
+const conteudoArquivoRepository = gerarArquivoRepository(
+  maiusculoPlural,
+  minusculoSingular,
+  tituloPlural,
+  tituloSingular
+)
+
+const textareaRoute = document.querySelector('section#resultados textarea#route')
+const textareaController = document.querySelector('section#resultados textarea#controller')
+const textareaRepository = document.querySelector('section#resultados textarea#repository')
+
+textareaRoute.innerHTML = conteudoArquivoRota
+textareaController.innerHTML = conteudoArquivoController
+textareaRepository.innerHTML = conteudoArquivoRepository
